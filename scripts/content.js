91f2009a2f116d783a3738d1e538f85cd60d5175
Min Yin <minyin142p@gmail.com>
2018-02-12 23:10:51 +0100

    create style

--- scripts/content.js
+++ scripts/content.js
@@ -4,21 +4,23 @@ var styleID = "browserscreen_VideoStyleID";
 chrome.runtime.onMessage.addListener(function(msg){
 
 	if(msg.message == "button_clicked" && window == window.top){
-		var video = document.querySelector("#" + videoClass);
+		var style = document.querySelector("#" + styleID);
 
 		// Video Browser Screen
-		if(video == null){
-			var browserVideo = findVideo();
+		if(style == null){
+			var video = findVideo();
 
-			if(browserVideo == null){
+			if(video == null){
 				console.log("No Video could be found.");
 			}else{
-				console.log(browserVideo);
+				createMainStyle();
+				console.log(video);
 			}
 		}
 
 		// Video Restore Size
 		else{
+			removeMainStyle();
 			console.log("Restore Video");
 		}
 	}
@@ -27,5 +29,43 @@ chrome.runtime.onMessage.addListener(function(msg){
 
 // Find Video in Website: HTML5, 
 function findVideo(){
+	var vid = document.querySelector("video");
 
+	if(vid != null) return vid;
+
+	console.log("No HTML5 video found.");
+}
+
+// Create Main Styles used by Extension
+function createMainStyle(){
+	var style = document.createElement("style");
+	style.id = styleID;
+
+	var css = `
+		body, html{
+			overflow: hidden !important;
+		}
+		.`+videoClass+`{
+			position: fixed !important;
+			top: 0 !important;
+			left: 0 !important;
+			right: 0 !important;
+			bottom: 0 !important;
+			z-Index: 2147483647 !important;
+			width: 100% !important;
+			height: 100% !important;
+			max-width: 100% !important;
+			max-height: 100% !important;
+			transform: none !important;
+			background: black !important;
+		}
+	`;
+	style.innerHTML = css;
+
+	document.querySelector("head").appendChild(style);
+}
+
+function removeMainStyle(){
+	var style = document.querySelector("#" + styleID);
+	if(style != null) style.parentNode.removeChild(style);
 }
