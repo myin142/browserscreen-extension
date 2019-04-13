895706f1f4bee54bb9e34e037938d9b8879a54e2
Min <minyin142p@gmail.com>
2019-04-13 07:44:52 +0200

    reindent with 4 spaces

--- scripts/background.js
+++ scripts/background.js
@@ -1,31 +1,31 @@
 // Cross Browser Support
 window.browser = (function(){
-	return window.msBrowser ||
-		window.browser ||
-		window.chrome
+    return window.msBrowser ||
+        window.browser ||
+        window.chrome
 })();
 
 browser.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-	var first = false;
+    var first = false;
 
-	// Resize first found Video
-	if(message.found && !first){
-		first = true;
-		sendResponse();
-		return true;
-	}
+    // Resize first found Video
+    if(message.found && !first){
+        first = true;
+        sendResponse();
+        return true;
+    }
 
-	// Search for sub frame in other frames
-	if(message.subWindow) browser.tabs.sendMessage(sender.tab.id, {subWindow: message.subWindow});
+    // Search for sub frame in other frames
+    if(message.subWindow) browser.tabs.sendMessage(sender.tab.id, {subWindow: message.subWindow});
 
-	// Send Message to Resize/Restore
-	if(message.resize) browser.tabs.sendMessage(sender.tab.id, {resize: true});
-	if(message.restore) browser.tabs.sendMessage(sender.tab.id, {restore: true});
+    // Send Message to Resize/Restore
+    if(message.resize) browser.tabs.sendMessage(sender.tab.id, {resize: true});
+    if(message.restore) browser.tabs.sendMessage(sender.tab.id, {restore: true});
 });
 
 browser.browserAction.onClicked.addListener(function(tab){
-	browser.tabs.query({active: true, currentWindow: true}, function(tabs){
-		// Resize Video
-		browser.tabs.sendMessage(tabs[0].id, {start: true});
-	});
+    browser.tabs.query({active: true, currentWindow: true}, function(tabs){
+        // Resize Video
+        browser.tabs.sendMessage(tabs[0].id, {start: true});
+    });
 });
--- scripts/content.js
+++ scripts/content.js
@@ -1,8 +1,8 @@
 // Cross Browser Support
 window.browser = (function(){
-	return window.msBrowser ||
-		window.browser ||
-		window.chrome
+    return window.msBrowser ||
+        window.browser ||
+        window.chrome
 })();
 
 var debugging = false;
@@ -17,107 +17,107 @@ var mControls = null;
 
 document.addEventListener("webkitfullscreenchange", (event) => {
 
-	if(document.webkitFullscreenElement){
-		let style = document.querySelector("#" + styleID);
-		if(style == null){
-			document.webkitExitFullscreen();
+    if(document.webkitFullscreenElement){
+        let style = document.querySelector("#" + styleID);
+        if(style == null){
+            document.webkitExitFullscreen();
 
-			let elem = document.webkitFullscreenElement;
-			let vid = findVideos(elem);
-			resizeElements(vid, false);
-			addToParents(elem, overlayClass);
-		}
-	}
+            let elem = document.webkitFullscreenElement;
+            let vid = findVideos(elem);
+            resizeElements(vid, false);
+            addToParents(elem, overlayClass);
+        }
+    }
 });
 
 browser.runtime.onMessage.addListener(function(msg){
-	if(debugging) console.log("Window: " + window.location.href);
+    if(debugging) console.log("Window: " + window.location.href);
 
-	// Check if resize or restore
-	if(msg.start && window == window.top){
-		if(debugging) console.log("Checking Resize/Restore");
+    // Check if resize or restore
+    if(msg.start && window == window.top){
+        if(debugging) console.log("Checking Resize/Restore");
 
-		// Detect active BrowerScreen by Main Style
-		var style = document.querySelector("#" + styleID);
-		if(style == null){
-			browser.runtime.sendMessage({resize: true});
-		}else{
-			browser.runtime.sendMessage({restore: true});
-		}
-	}
+        // Detect active BrowerScreen by Main Style
+        var style = document.querySelector("#" + styleID);
+        if(style == null){
+            browser.runtime.sendMessage({resize: true});
+        }else{
+            browser.runtime.sendMessage({restore: true});
+        }
+    }
 
-	// Resize Video
-	else if(msg.resize){
-		if(debugging) console.log("Searching in Window");
+    // Resize Video
+    else if(msg.resize){
+        if(debugging) console.log("Searching in Window");
 
-		var vid = findVideos(document);
-		if(vid == null){
-			if(debugging) console.log("No Videos could be found.");
-			return;
-		}
+        var vid = findVideos(document);
+        if(vid == null){
+            if(debugging) console.log("No Videos could be found.");
+            return;
+        }
 
-		// Found a Video
-		browser.runtime.sendMessage({found: true}, function(response){
-			if(debugging){
-				console.log("Resize Video");
-				console.log(vid);
-			}
+        // Found a Video
+        browser.runtime.sendMessage({found: true}, function(response){
+            if(debugging){
+                console.log("Resize Video");
+                console.log(vid);
+            }
 
-			// Resize Video and all top frames
-			vid.classList.add(videoClass);
-			resizeElements(vid);
+            // Resize Video and all top frames
+            vid.classList.add(videoClass);
+            resizeElements(vid);
 
-			// Create Video Controls for HTML5 Videos
-			if(vid.tagName == "VIDEO"){
-				if(debugging) console.log("Creating Controls");
-				createControls(vid);
-			}
-		});
-	}
+            // Create Video Controls for HTML5 Videos
+            if(vid.tagName == "VIDEO"){
+                if(debugging) console.log("Creating Controls");
+                createControls(vid);
+            }
+        });
+    }
 
-	// Restore Video
-	else if(msg.restore){
-		if(debugging) console.log("Restore Video");
+    // Restore Video
+    else if(msg.restore){
+        if(debugging) console.log("Restore Video");
 
-		// Restore Elements and Remove Controls
-		var vid = document.querySelector("." + videoClass);
-		restoreElements();
-		removeControls();
+        // Restore Elements and Remove Controls
+        var vid = document.querySelector("." + videoClass);
+        restoreElements();
+        removeControls();
 
-		if(vid != null){
-			vid.classList.remove(videoClass);
-		}
-	}
+        if(vid != null){
+            vid.classList.remove(videoClass);
+        }
+    }
 
-	// On Message from an IFRAME/OBJECT
-	else if(msg.subWindow){
-		if(debugging) console.log("Search Sub Window");
-		var link = msg.subWindow;
+    // On Message from an IFRAME/OBJECT
+    else if(msg.subWindow){
+        if(debugging) console.log("Search Sub Window");
+        var link = msg.subWindow;
 
-		// Search Iframes
-		var iframe = searchIFrames(link);
-		if(iframe != null){
-			resizeElements(iframe);
-			if(debugging){
-				console.log("Found Iframe: ");
-				console.log(iframe);
-			}
-			return;
-		}
+        // Search Iframes
+        var iframe = searchIFrames(link);
+        if(iframe != null){
+            resizeElements(iframe);
+            if(debugging){
+                console.log("Found Iframe: ");
+                console.log(iframe);
+            }
+            return;
+        }
 
-		// Search Objects
-		var object = searchObjects(link);
-		if(object != null){
-			resizeElements(object);
-			if(debugging){
-				console.log("Found Object: ");
-				console.log(object);
-			}
-			return;
-		}
+        // Search Objects
+        var object = searchObjects(link);
+        if(object != null){
+            resizeElements(object);
+            if(debugging){
+                console.log("Found Object: ");
+                console.log(object);
+            }
+            return;
+        }
 
-		if(debugging) console.log("Nothing Found");
-	}
+        if(debugging) console.log("Nothing Found");
+    }
 
 });
 
@@ -126,112 +126,112 @@ function createControls(video){
 }
 
 function removeControls(){
-	if(mControls != null){
-		if(debugging) console.log("Removing Controls");
-		mControls.removeControls();
-		mControls = null;
-	}
+    if(mControls != null){
+        if(debugging) console.log("Removing Controls");
+        mControls.removeControls();
+        mControls = null;
+    }
 }
 
 // Search for Link in IFRAMEs
 function searchIFrames(link){
-	var iframes = document.querySelectorAll("iframe");
-	for(var i = 0; i < iframes.length; i++){
-		var elemSrc = getFormattedSource(iframes[i].src);
-		var msgSrc = getFormattedSource(link);
+    var iframes = document.querySelectorAll("iframe");
+    for(var i = 0; i < iframes.length; i++){
+        var elemSrc = getFormattedSource(iframes[i].src);
+        var msgSrc = getFormattedSource(link);
 
-		if(elemSrc == msgSrc)
-			return iframes[i];
-	}
+        if(elemSrc == msgSrc)
+            return iframes[i];
+    }
 }
 
 // Search for Link in OBJECTs
 function searchObjects(link){
-	var objects = document.querySelectorAll("object");
-	for(var i = 0; i < objects.length; i++){
-		var elemSrc = getFormattedSource(objects[i].data);
-		var msgSrc = getFormattedSource(link);
+    var objects = document.querySelectorAll("object");
+    for(var i = 0; i < objects.length; i++){
+        var elemSrc = getFormattedSource(objects[i].data);
+        var msgSrc = getFormattedSource(link);
 
-		if(elemSrc == msgSrc)
-			return objects[i];
-	}
+        if(elemSrc == msgSrc)
+            return objects[i];
+    }
 }
 
 // Remove Styles and Fullscreen Classes
 function restoreElements(){
-	removeMainStyle();
+    removeMainStyle();
 
-	// Remove Fullscreen Classes
-	var elems = document.querySelectorAll("."+fullscreenClass);
-	for(var i = 0; i < elems.length; i++){
-		elems[i].classList.remove(fullscreenClass);
-		elems[i].classList.remove(overlayClass);
-	}
+    // Remove Fullscreen Classes
+    var elems = document.querySelectorAll("."+fullscreenClass);
+    for(var i = 0; i < elems.length; i++){
+        elems[i].classList.remove(fullscreenClass);
+        elems[i].classList.remove(overlayClass);
+    }
 
-	// Fix Youtube offset error
-	window.dispatchEvent(new Event("resize"));
+    // Fix Youtube offset error
+    window.dispatchEvent(new Event("resize"));
 }
 
 function addToParents(elem, className){
-	while(elem != null && elem.classList != undefined){
-		elem.classList.add(className);
-		elem = elem.parentNode;
-	}
+    while(elem != null && elem.classList != undefined){
+        elem.classList.add(className);
+        elem = elem.parentNode;
+    }
 }
 
 // Add Fullscreen Classes to all Parents of Video and message top frames
 function resizeElements(elem, overlay = true){
 
-	// Resize all Parents
-	while(elem != null && elem.classList != undefined){
-		elem.classList.add(fullscreenClass);
+    // Resize all Parents
+    while(elem != null && elem.classList != undefined){
+        elem.classList.add(fullscreenClass);
 
-		if(overlay){
-			elem.classList.add(overlayClass);
-		}
-		elem = elem.parentNode;
-	}
+        if(overlay){
+            elem.classList.add(overlayClass);
+        }
+        elem = elem.parentNode;
+    }
 
-	createMainStyle();
+    createMainStyle();
 
-	// Resize all top frames
-	if(window != window.top){
-		if(debugging) console.log("Searching for frame: " + window.location.href);
-		browser.runtime.sendMessage({subWindow: window.location.href});
-	}
+    // Resize all top frames
+    if(window != window.top){
+        if(debugging) console.log("Searching for frame: " + window.location.href);
+        browser.runtime.sendMessage({subWindow: window.location.href});
+    }
 }
 
 // Format Link to prevent mistakes with http/https
 function getFormattedSource(src){
-	return src.replace(/^https?\:\/\//i, "").replace(/^http?\:\/\//i, "");
+    return src.replace(/^https?\:\/\//i, "").replace(/^http?\:\/\//i, "");
 }
 
 // Find Videos in Website: FLASH, HTML5, EMBEDED. Flash before HTML5 for Crunchyroll
 function findVideos(root){
-	// Search Flash Videos
-	var vid = root.querySelector("video");
+    // Search Flash Videos
+    var vid = root.querySelector("video");
 
-	// Search HTML5 Videos
-	if(vid == null){
-		vid = root.querySelector("object[type='application/x-shockwave-flash']");
+    // Search HTML5 Videos
+    if(vid == null){
+        vid = root.querySelector("object[type='application/x-shockwave-flash']");
 
-		// Search EMBEDed Videos
-		if(vid == null){
-			vid = root.querySelector("embed");
-		}
-	}
+        // Search EMBEDed Videos
+        if(vid == null){
+            vid = root.querySelector("embed");
+        }
+    }
 
-	return vid;
+    return vid;
 }
 
 // Create Main Styles used by Extension
 function createMainStyle(){
-	if(document.querySelector("#" + styleID) != null) return;
+    if(document.querySelector("#" + styleID) != null) return;
 
-	var style = document.createElement("style");
-	style.id = styleID;
+    var style = document.createElement("style");
+    style.id = styleID;
 
-	var css = `
+    var css = `
 		::-webkit-scrollbar{
 			display: none;
 		}
@@ -262,13 +262,13 @@ function createMainStyle(){
 			z-Index: 2147483647;
 		}
 	`;
-	style.innerHTML = css;
+    style.innerHTML = css;
 
-	document.querySelector("head").appendChild(style);
+    document.querySelector("head").appendChild(style);
 }
 
 // Remove Main Style
 function removeMainStyle(){
-	var style = document.querySelector("#" + styleID);
-	if(style != null) style.parentNode.removeChild(style);
+    var style = document.querySelector("#" + styleID);
+    if(style != null) style.parentNode.removeChild(style);
 }
